eb236d65b2c573fef3679d875d9b9707c90e61d2
artog <adam.ingmansson@gmail.com>
2016-06-21 21:53:53 +0200

    Added OpenGL Libs

--- Photon Mapper/stdafx.h
+++ Photon Mapper/stdafx.h
@@ -5,11 +5,26 @@
 
 #pragma once
 
+#ifdef WIN32
+#include <windows.h>
+#endif
+
 #include "targetver.h"
 
 #include <stdio.h>
 #include <tchar.h>
 
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+
+#include <string.h>
+
+// GL stuff
+#include "libs\glew\glew.h"
+#include "libs\freeglut\glut.h"
+#include "libs\freeglut\freeglut.h"
+#include "libs\glutil.h"
 
 
 // TODO: reference additional headers your program requires here
